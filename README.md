6e7570d901a76eb2135e4c3a1b32cd5dee5fcb14
Uros <uros.tripunovic@omegapoint.se>
2025-02-14 22:42:11 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# zod_validation_fetch
+
+Install bun on [your machine](https://bun.sh/docs/installation#installing)
+
+
+To install dependencies:
+
+```bash
+bun install
+```
+
+To run server:
+
+```bash
+bun run dev
+```
+
+To run fetch script:
+
+```bash
+bun run fetch
+```
+
+This project was created using `bun init` in bun v1.1.3. [Bun](https://bun.sh) is a fast all-in-one JavaScript runtime.
---
+++ fetch.ts
@@ -0,0 +1,39 @@
+import { z } from "zod";
+
+const jwtSchema = z.object({
+  access_token: z.string().jwt()
+}).passthrough()
+
+type JWT = z.infer<typeof jwtSchema>;
+
+const apiErrorSchema = z.object({
+  error: z.string(),
+  error_description: z.optional(z.string())
+})
+
+const fetchAccessToken = async (flag: boolean): Promise<JWT | 'Server down' | 'Invalid api call' | 'Unexpected api error'> => {
+  try {
+    const res = await fetch(`localhost:3000/endpoint?flag=${flag}`)
+    const json = await res.json()
+
+    if (jwtSchema.safeParse(json).success) {
+      return json.access_token
+    }
+
+    const not_ok = apiErrorSchema.safeParse(json);
+    if (not_ok.success) {
+      console.error(res.status, not_ok.data)
+      return 'Invalid api call'
+    }
+
+    return 'Unexpected api error';
+  } catch (error) {
+    console.error(error)
+    return 'Server down'
+  }
+}
+
+//Status code 200
+console.log('\x1b[38;5;46m%s\x1b[0m', await fetchAccessToken(true));
+//Status code greater than 299
+console.log('\x1b[38;5;214m%s\x1b[0m', await fetchAccessToken(false));
