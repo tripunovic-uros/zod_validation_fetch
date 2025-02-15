578b8971f57a44346449e152579b570050fcb364
Uros <uros.tripunovic@omegapoint.se>
2025-02-15 14:33:06 +0100

    format code with denols

--- fetch.ts
+++ fetch.ts
@@ -1,39 +1,43 @@
 import { z } from "zod";
 
 const jwtSchema = z.object({
-  access_token: z.string().jwt()
-}).passthrough()
+  access_token: z.string().jwt(),
+}); //.passthrough() //if we want to passthrough uknown keys
 
 type JWT = z.infer<typeof jwtSchema>;
 
 const apiErrorSchema = z.object({
   error: z.string(),
-  error_description: z.optional(z.string())
-})
+  error_description: z.optional(z.string()),
+});
 
-const fetchAccessToken = async (flag: boolean): Promise<JWT | 'Server down' | 'Invalid api call' | 'Unexpected api error'> => {
+const fetchAccessToken = async (
+  flag: boolean,
+): Promise<
+  JWT | "Server down" | "Invalid api call" | "Unexpected api error"
+> => {
   try {
-    const res = await fetch(`localhost:3000/endpoint?flag=${flag}`)
-    const json = await res.json()
+    const res = await fetch(`localhost:3000/endpoint?flag=${flag}`);
+    const json = await res.json();
 
     if (jwtSchema.safeParse(json).success) {
-      return json.access_token
+      return json.access_token;
     }
 
     const not_ok = apiErrorSchema.safeParse(json);
     if (not_ok.success) {
-      console.error(res.status, not_ok.data)
-      return 'Invalid api call'
+      console.error(res.status, not_ok.data);
+      return "Invalid api call";
     }
 
-    return 'Unexpected api error';
+    return "Unexpected api error";
   } catch (error) {
-    console.error(error)
-    return 'Server down'
+    console.error(error);
+    return "Server down";
   }
-}
+};
 
 //Status code 200
-console.log('\x1b[38;5;46m%s\x1b[0m', await fetchAccessToken(true));
+console.log("\x1b[38;5;46m%s\x1b[0m", await fetchAccessToken(true));
 //Status code greater than 299
-console.log('\x1b[38;5;214m%s\x1b[0m', await fetchAccessToken(false));
+console.log("\x1b[38;5;214m%s\x1b[0m", await fetchAccessToken(false));
--- server.ts
+++ server.ts
@@ -2,18 +2,22 @@
 // 200 returns a access token with other fields
 // >300 returns an error json object
 
-
 const server = Bun.serve({
   port: 3000,
   fetch(req) {
     const url = new URL(req.url);
     if (url.pathname === "/endpoint") {
-      const flag = url.searchParams.get('flag')
-      if (flag === 'true') {
-        const jwt = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c" // from JWT.io, no private info here.
-        return Response.json({ access_token: jwt, tokyo: "drift" }, { status: 200 })
+      const flag = url.searchParams.get("flag");
+      if (flag === "true") {
+        const jwt =
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"; // from JWT.io, no private info here.
+        return Response.json({ access_token: jwt, tokyo: "drift" }, {
+          status: 200,
+        });
       }
-      return Response.json({ error: "error", error_description: "some desc" }, { status: 400 })
+      return Response.json({ error: "error", error_description: "some desc" }, {
+        status: 400,
+      });
     }
     return new Response("Page not found!", { status: 404 });
   },
